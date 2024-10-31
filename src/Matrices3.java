92804d8d2e7da307d0b6b1508fbf48750706b35d
LUIS GERARDO LOPEZ HERNANDEZ <l24121302@morelia.tecnm.mx>
2024-10-31 13:55:33 -0600

    clase 4 de matrices

--- src/Matrices3.java
+++ src/Matrices3.java
@@ -2,25 +2,32 @@ import java.util.Scanner;
 
 public class Matrices3 {
     static Scanner escaner = new Scanner(System.in);
-    static final int FILAS = 3, COLUMNAS = 4;
+    static final int FILAS = 3, COLUMNAS = 4, EXIT_CODE = 10, ERROR_CODE = 0;
+    static final String TEXTO_PARA_CUBICULO_VACIO = "(Vacío)";
     static String[][] cubiculos = new String[FILAS][COLUMNAS];
     public static void main(String[] args) {
-        int opcion = getOpcion();
-        while (opcion != 4) {
+        int opcion;
+        do {
+            opcion = getOpcion();
             switch (opcion) {
                 case 1: reservaCubiculo();
                     break;
                 case 2: liberaCubiculo();
                     break;
-                case 3: consultaCubiculo();
+                case 3: mostrarCubiculos();
                     break;
-                case 4: terminaPrograma();
+                case 4: verificarReservacion();
+                    break;
+                case 5: consultaCubiculo();
+                    break;
+                case ERROR_CODE: System.out.println("¡Opción inválida!");
+                    break;
+                case EXIT_CODE: terminaPrograma();
                     break;
                 default:
-                    System.out.println("¡Opción inválida!");
+                    System.out.println("¡Opción fuera del rango!");
             }
-            opcion = getOpcion();
-        }
+        } while (opcion != EXIT_CODE);
     }
 
     static void terminaPrograma() {
@@ -28,11 +35,25 @@ public class Matrices3 {
         escaner.close();
     }
 
+    static void verificarReservacion() {
+        System.out.println("Verifica reservación:");
+        System.out.print("Ingrese el nombre del estudiante: ");
+        String nombre = escaner.nextLine().toUpperCase();
+        String filaEncontrada = null, columnaEncontrada = null;
+        for (int i = 0; i < FILAS; i++)
+            for (int j = 0; j < COLUMNAS; j++)
+                if (!estaVacio(i, j) && cubiculos[i][j].equals(nombre)){
+                    filaEncontrada = i + "";
+                    columnaEncontrada = j + "";
+                }
+        System.out.println(filaEncontrada == null ? nombre + " no ha reservado ningun cubiculo" : "el cubiculo de " + nombre + " tiene coordanadas " + filaEncontrada + " y " + columnaEncontrada);
+    }
+
     static void consultaCubiculo() {
         System.out.println("Consulta cubículo:");
         int[] coordenadas = getCoordenadasValidas();
         if (coordenadas != null) {
-            if (!cubiculoVacio(coordenadas[0], coordenadas[1]))
+            if (!estaVacio(coordenadas[0], coordenadas[1]))
                 System.out.println("¡Cubículo ocupado por: " + cubiculos[coordenadas[0]][coordenadas[1]] + "!");
             else System.out.println("¡Cubículo vacío!");
         } else System.out.println("¡Coordenadas inválidas!");
@@ -42,29 +63,53 @@ public class Matrices3 {
         System.out.println("Libera cubículo:");
         int[] coordenadas = getCoordenadasValidas();
         if (coordenadas != null) {
-            if (!cubiculoVacio(coordenadas[0], coordenadas[1])) {
+            if (!estaVacio(coordenadas[0], coordenadas[1])) {
                 cubiculos[coordenadas[0]][coordenadas[1]] = null;
                 System.out.println("¡Cubículo liberado correctamente!");
             } else System.out.println("¡Cubículo vacío!");
         } else System.out.println("¡Coordenadas inválidas!");
     }
 
-    static void imprimeCubiculos() {
+    static void mostrarCubiculos() {
+        int masLargo = longitudDelNombreMasLargo();
         System.out.println("\nCubículos:");
         for (int i = 0; i < FILAS; i++) {
             for (int j = 0; j < COLUMNAS; j++)
-                System.out.print(cubiculoVacio(i, i) ? "(Vacío)\t\t" : cubiculos[i][j] + "\t");
+                System.out.print(getCubiculoTabulado(i, j, masLargo));
             System.out.println();
         }
+    }
+    
+    static int longitudDelNombreMasLargo() {
+        int masLargo = TEXTO_PARA_CUBICULO_VACIO.length();
+        for (int i = 0; i < FILAS; i++)
+            for (int j = 0; j < COLUMNAS; j++)
+                if (!estaVacio(i, j) && cubiculos[i][j].length() > masLargo)
+                    masLargo = cubiculos[i][j].length();
+        return masLargo;
+    }
+    
+    static String getCubiculoTabulado(int fila, int columna, int masLargo) {
+        String cubiculo = estaVacio(fila, columna) ? TEXTO_PARA_CUBICULO_VACIO : cubiculos[fila][columna];
+        int tabsNecesarios = 1;
+        if (cubiculo.length() < masLargo)
+            tabsNecesarios = ((masLargo - cubiculo.length()) / 4) + 1;
+        for (int i = 0; i < tabsNecesarios; i++) 
+            cubiculo += "\t";
+        return cubiculo;
+    }
+    
+    static String getNombreTabulado(String nombre) {
+        return nombre.length() > "(Vacío)".length() ? nombre + "\t" : nombre + "\t\t";
     }
 
     static void reservaCubiculo() {
         System.out.println("Reserva cubículo:");
         int[] coordenadas = getCoordenadasValidas();
         if (coordenadas != null) {
-            if (cubiculoVacio(coordenadas[0], coordenadas[1])) {
-                System.out.println("Ingrese el nombre del estudiante");
-                String nombre = escaner.nextLine();
+            if (estaVacio(coordenadas[0], coordenadas[1])) {
+                System.out.print("Ingrese el nombre del estudiante: ");
+                String nombre = escaner.nextLine().toUpperCase();
                 cubiculos[coordenadas[0]][coordenadas[1]] = nombre;
                 System.out.println("¡Cubículo reservado correctamente!");
             } else System.out.println("¡Cubículo ocupado!");
@@ -72,11 +117,14 @@ public class Matrices3 {
     }
 
     static int getOpcion() {
-        imprimeCubiculos();
-        System.out.print("Menu\n1. Reserva cubiculo\n2. Libera cubiculo\n3. Consulta cubiculo\n4. Salir\n>> ");
-        int opcion = escaner.nextInt();
-        escaner.nextLine(); // Limpiar el buffer
-        return opcion;
+        try {
+            System.out.print("\nMenu\n1. Reserva cubiculo\n2. Libera cubiculo\n3. Mostrar cubiculo\n4. Consulta cubiculo\n" + EXIT_CODE + ". Salir\n>> ");
+            return escaner.nextInt();
+        } catch(Exception e) {
+            return 0;
+        } finally {
+            escaner.nextLine(); // Limpiar el buffer
+        }
     }
 
     static int[] getCoordenadasValidas() {
@@ -88,7 +136,7 @@ public class Matrices3 {
         } else return null;
     }
 
-    static boolean cubiculoVacio(int fila, int columna) {
+    static boolean estaVacio(int fila, int columna) {
         return cubiculos[fila][columna] == null;
     }
 }
