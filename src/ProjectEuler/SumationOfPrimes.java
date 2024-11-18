1f34b0905b7390c0dd3978ca68ce5d6fc3086f33
gamaerry <gamaerry@gmail.com>
2024-11-18 11:29:25 -0600

    suma de todos los primos menores a 2_000_000

---
+++ src/ProjectEuler/SumationOfPrimes.java
@@ -0,0 +1,33 @@
+package ProjectEuler;
+
+import java.util.ArrayList;
+
+public class SumationOfPrimes {
+    public static void main(String[] args) {
+        optimizada();
+    }
+    static void optimizada() {
+        ArrayList<Integer> listaDePrimos = new ArrayList<>();
+        boolean actualEsPrimo = true;
+        int actual = 9;
+        listaDePrimos.add(3);
+        listaDePrimos.add(5);
+        listaDePrimos.add(7); // el 2 puede omitirse ya que ninguno de los números impares es divisible por 2
+        int p = listaDePrimos.get(0);
+        long suma = 17;
+        for (int i = 0; actual < 2_000_000; actualEsPrimo = true, i = 0) {
+            p = listaDePrimos.get(i);
+            actual += 2; // inicia corroborando si 11 (el 5to) es primo
+            while (p * p <= actual)
+                if (actual % p == 0){
+                    actualEsPrimo = false;
+                    break;
+                } else p = listaDePrimos.get(++i);
+            if (actualEsPrimo){
+                listaDePrimos.add(actual);
+                suma += actual;
+            }
+        }
+        System.out.println(suma);
+    }
+}
