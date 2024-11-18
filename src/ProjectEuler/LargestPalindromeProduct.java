4b2bc03d7cd7c98ad57422447990088cc6ad3938
gamaerry <gamaerry@gmail.com>
2024-11-17 21:58:40 -0600

    palindromo mas largo como producto de dos de tres digitos

---
+++ src/ProjectEuler/LargestPalindromeProduct.java
@@ -0,0 +1,24 @@
+package ProjectEuler;
+
+public class LargestPalindromeProduct {
+    public static void main(String[] args) {
+        int palindromo = 0;
+        boolean palindromoEncontrado = false;
+        for (int i = 999; i > 99; i--){
+            for (int j = i; j > 99 && !palindromoEncontrado; j--) {
+                palindromoEncontrado = esPalindromo(i*j);
+                if (palindromoEncontrado && j*i >palindromo)
+                    palindromo =i*j;
+            }
+            palindromoEncontrado = false;
+        }
+        System.out.println(palindromo);
+    }
+    static boolean esPalindromo(int n) {
+        String s = n + "";
+        for (int i = 0; i < s.length()/2; i++) 
+            if (!(s.charAt(i) == s.charAt(s.length() - (i+1))))
+                return false;
+        return true;
+    }
+}
