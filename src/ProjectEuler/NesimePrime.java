1955382224950718621c1219da7df1422a8d0976
gamaerry <gamaerry@gmail.com>
2024-11-18 21:41:01 -0600

    algoritmo primo optimizado

--- src/ProjectEuler/NesimePrime.java
+++ src/ProjectEuler/NesimePrime.java
@@ -3,42 +3,29 @@ package ProjectEuler;
 import java.util.ArrayList;
 
 public class NesimePrime {
-    static final int N = 50_000_000;
+    static final int N = 5_000_000;
     public static void main(String[] args) {
         optimizada();
     }
 
-    static void simple() {
-        boolean actualEsPrimo = true;
-        int actual = 9; 
-        for (int n = 4, i = 3; n < N; i = 3, actualEsPrimo = true) {
-            actual += 2; // inicia corroborando si 11 (el 5to) es primo
-            while (i * i <= actual)
-                if (actual % i == 0){
-                    actualEsPrimo = false;
-                    break;
-                } else i += 2;
-            if (actualEsPrimo) n++;
-        }
-        System.out.println(actual);
-    }
-
     static void optimizada() {
-        ArrayList<Integer> listaDePrimos = new ArrayList<>();
-        boolean actualEsPrimo = true;
-        int actual = 9;
+        final ArrayList<Integer> listaDePrimos = new ArrayList<>();
         listaDePrimos.add(3);
         listaDePrimos.add(5);
         listaDePrimos.add(7); // el 2 puede omitirse ya que ninguno de los números impares es divisible por 2
-        int p = listaDePrimos.get(0);
-        for (int n = 4, i = 0; n < N; actualEsPrimo = true, i = 0) {
-            p = listaDePrimos.get(i);
+        boolean actualEsPrimo = true;
+        int actual = 9;
+        int n = 4;
+        while (n != N) {
+            actualEsPrimo = true;
             actual += 2; // inicia corroborando si 11 (el 5to) es primo
-            while (p * p <= actual)
+            for (int p : listaDePrimos) {
+                if (p * p > actual) break;
                 if (actual % p == 0){
                     actualEsPrimo = false;
                     break;
-                } else p = listaDePrimos.get(++i);
+                }
+            }
             if (actualEsPrimo){
                 listaDePrimos.add(actual);
                 n++;
