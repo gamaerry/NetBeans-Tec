cd78f39e0d91168b98f05b1566e5ba7ebc3c32c1
gamaerry <gamaerry@gmail.com>
2024-11-24 21:37:59 -0600

    caminos posibles en un tablero de 20x20

---
+++ src/ProjectEuler/LatticePaths.java
@@ -0,0 +1,14 @@
+package ProjectEuler;
+
+import java.math.BigInteger;
+
+public class LatticePaths {
+    public static void main(String[] args) {
+        System.out.println(factorial(BigInteger.valueOf(40)).divide(factorial(BigInteger.valueOf(20)).pow(2)));
+    }
+    static BigInteger factorial (BigInteger n) {
+        if (n.compareTo(BigInteger.TWO) < 0) 
+            return BigInteger.ONE;
+        return n.multiply(factorial(n.add(BigInteger.ONE.negate())));
+    }
+}
